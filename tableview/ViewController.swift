1b3abfb573c2b1d9b6c74a289eb7865b36af2dab
Avinashg137 <avinashg.itpath@gmail.com>
2022-03-04 14:00:43 -0800

    Initial Commit

---
+++ tableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableview
+//
+//  Created by MAC OS on 04/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
